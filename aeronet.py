4d7fca526dd9f7dd053416fb558b6af2370a140d
samuelleblanc <samuel.elie.leblanc@gmail.com>
2016-06-06 17:47:39 -0700

    updates to v0.8 beta some fixes, added saving to ict

--- aeronet.py
+++ aeronet.py
@@ -31,9 +31,9 @@ def get_aeronet(daystr=None):
         daystr = dd
     else:
         if daystr > dd:
-	    daystr = dd
-	    import warnings
-	    warnings.warn("Date set to future, using today's date")
+            daystr = dd
+            import warnings
+            warnings.warn("Date set to future, using today's date")
     url = 'http://aeronet.gsfc.nasa.gov/cgi-bin/print_web_data_v2_globe?year={yyyy}&month={mm}&day={dd}&year2={yyyy}&month2={mm}&day2={dd}&LEV10=1&AVG=20'.format(yyyy=daystr[0:4],mm=int(daystr[5:7]),dd=int(daystr[8:10]))
     print 'Getting file from internet: at aeronet.gsfc.nasa.gov'
     print url
@@ -42,21 +42,21 @@ def get_aeronet(daystr=None):
         soup = BeautifulSoup(html)
     except:
         print 'failed to communicate with internet returning nothing'
-	return False
+        return False
     lines = []
     for br in soup.findAll('br'):
         nt = br.nextSibling
-	lines.append(nt.strip()+'\n')
+        lines.append(nt.strip()+'\n')
     s = StringIO(''.join(lines))
     s.seek(0)
     try:
         dat = np.genfromtxt(s,delimiter=',',names=True,dtype=None)
     except IndexError:
         print 'Failed to read the returned html file'
-	return False
+        return False
     fields_to_ignore = ['AERONET_Site_Name','Principal_Investigator','PI_Email','Dateddmmyy']
     for label in dat.dtype.names:
         if not label in fields_to_ignore:
-	    if dat[label].dtype.type is np.str_:
-	         dat[label] = np.genfromtxt(dat[label])
+            if dat[label].dtype.type is np.str_:
+                dat[label] = np.genfromtxt(dat[label])
     return dat
